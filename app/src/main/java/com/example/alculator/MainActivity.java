c484aa799eed18a3cb986ec197589f96a508e03e
temp <bakytova120496@gmail.ru>
2019-09-12 15:04:44 +0600

    Калькулятор Аки :3

--- app/src/main/java/com/example/alculator/MainActivity.java
+++ app/src/main/java/com/example/alculator/MainActivity.java
@@ -3,33 +3,92 @@ package com.example.alculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    final int MENU_RESET_ID = 1;
+
+
+    EditText Num1;
+    EditText Num2;
+    TextView Result;
+    Button sun;
+    Button min;
+    Button multi;
+    Button div;
+
+    String oper = "";
+    private Menu menu;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public  void onButtonClick (View v){
 
-        EditText el1 = (EditText)findViewById(R.id.Num1);
-        EditText el2 = (EditText)findViewById(R.id.Num2);
-        TextView resText = (TextView)findViewById(R.id.Result);
 
-        int num1 = Integer.parseInt(el1.getText().toString());
-        int num2 = Integer.parseInt(el2.getText().toString());
-        int resSum = num1 + num2;
-        resText.setText(Integer.toString(resSum));
-        int min = num1 - num2;
-        resText.setText(Integer.toString(min));
-        int multi = num1*num2;
-        resText.setText(Integer.toString(multi));
-        int div = num1/num2;
-        resText.setText(Integer.toString(div));
+    Num1 = findViewById(R.id.Num1);
 
+    Num2 = findViewById(R.id.Num2);
+
+
+    sun =findViewById(R.id.sun);
+
+    min =findViewById(R.id.min);
+
+    multi =findViewById(R.id.min);
+
+    div =findViewById(R.id.div);
+
+    Result =findViewById(R.id.Result);
+
+
+
+}
+
+    public void onBtnClick(View v) {
+        float num1 = 0;
+        float num2 = 0;
+        float result = 0;
+
+
+//        if (TextUtils.isEmpty(Num1.getText().toString())
+//                || TextUtils.isEmpty(Num2.getText().toString())) {
+//            return;
+//        }
+
+
+        num1 = Float.parseFloat(Num1.getText().toString());
+        num2 = Float.parseFloat(Num2.getText().toString());
+
+
+        switch (v.getId()) {
+            case R.id.sun:
+                oper = "+";
+                result = num1 + num2;
+                break;
+            case R.id.min:
+                oper = "-";
+                result = num1 - num2;
+                break;
+            case R.id.multi:
+                oper = "*";
+                result = num1 * num2;
+                break;
+            case R.id.div:
+                oper = "/";
+                result = num1 / num2;
+                break;
+            default:
+                break;
+        }
+
+
+        Result.setText(num1 + " " + oper + " " + num2 + " = " + result);
     }
 }
